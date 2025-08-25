d78c6a8b1da072f262f8307408f859d7f5572a14
Roni Kreinin <rkreinin@clearpathrobotics.com>
2025-08-25 15:37:42 -0400

    CPP generation

---
+++ protoncpp/Inc/proton.hpp
@@ -0,0 +1,19 @@
+/**
+ * Software License Agreement (proprietary)
+ *
+ * @copyright Copyright (c) 2025 Clearpath Robotics, Inc., All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is not permitted without the express permission of Clearpath
+ * Robotics.
+ *
+ * @author Roni Kreinin (rkreinin@clearpathrobotics.com)
+ */
+
+#ifndef INC_PROTON_PROTON_HPP_
+#define INC_PROTON_PROTON_HPP_
+
+#include "bundle.pb.h"
+
+
+#endif // INC_PROTON_PROTON_HPP_
---
+++ protoncpp/Src/proton.cpp
@@ -0,0 +1,15 @@
+/**
+ * Software License Agreement (proprietary)
+ *
+ * @copyright Copyright (c) 2025 Clearpath Robotics, Inc., All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is not permitted without the express permission of Clearpath
+ * Robotics.
+ *
+ * @author Roni Kreinin (rkreinin@clearpathrobotics.com)
+ */
+
+#include "proton.hpp"
+#include <stdio.h>
+
